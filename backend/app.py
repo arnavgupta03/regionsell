59ddd34316c197f78ed73e97850a1b7f6c8f8b5b
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2023-02-12 01:32:46 -0500

    Remove CORS from API once deployed

--- backend/app.py
+++ backend/app.py
@@ -5,10 +5,8 @@ from datetime import datetime
 import cohere
 from dotenv import load_dotenv
 from flask import Flask, request
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 load_dotenv()
 
