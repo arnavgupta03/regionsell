fa575f0211a9308f9365e10bd5ff766dd36c6bfe
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2023-02-11 22:24:27 -0500

    Add working main page for Chrome extension

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
   let [productName, setProductName] = React.useState<string>('');
+  let [suggestedList, setSuggestedList] = React.useState<{"label": string, "value": number}[]>([]);
 
-  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-      let url = tabs[0].url;
-      console.log(url);
-      // use `url` here inside the callback because it's asynchronous!
-      setProductName((url?.split('?')[1].split('&')[0].split('=')[1].split('+'))?.join(' ') ?? 'No product name found')
-  });
+  useEffect(() => {
+    // get current tab url
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let url = tabs[0].url;
+        console.log(url);
+        // use `url` here inside the callback because it's asynchronous!
+        setProductName((url?.split('?')[1].split('&')[0].split('=')[1].split('+'))?.join(' ') ?? 'No product name found')
+        // call home endpoint of flask api passing product name as query param
+        fetch(`http://localhost:5000/?item=${productName}`).then((response) => {
+          return response.json();
+        }).then((data) => {
+          console.log(data.slice(0, 5));
+          setSuggestedList(data.slice(0, 5));
+        });
+    });
+  }, [productName, suggestedList]);
   return (
-    <div className="App">
+    <div className="App p-3">
       <h1 className="display-1 text-center">RegionSell</h1>
-      <h2 className="display-4 text-center">Product: {productName}</h2>
+      <h2 className="display-4 text-center">Product: {
+      productName.toLowerCase()
+        .split(' ')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ')
+      }</h2>
+      <h3 className="lead text-center">Suggested Places to Sell this Product</h3>
+      <ul>
+        {suggestedList.map((suggestion, index) => {
+          return <li key={index}>{suggestion.label}</li>;
+        })}
+      </ul>
     </div>
   );
 }
